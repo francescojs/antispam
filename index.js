1c50d598d1ee7d0b8bb4229852741a045d51e11b
Francesco <72751485+francescojs@users.noreply.github.com>
2021-02-25 11:08:14 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('message', (message) => {
+    const logs = message.guild.channels.cache.get("734976829504618527");
+    const embed2 = new Discord.MessageEmbed()
+    .setTitle("NEW SPAM")
+    .setDescription(`${message.author} has posted a spam link in ${message.channel}\nContent was: ${message.content}`)
+    .setTimestamp();
+    const embed = new Discord.MessageEmbed()
+    .setColor("YELLOW")
+    .addField("Message deleted", `${message.author} spam isn't allowed.`) 
+    .setTimestamp();
+    if (message.content.includes('discord.gg/'||'discordapp.com/invite/')) { //if it contains an invite link
+      message.delete()
+        .then(message.channel.send(embed))
+        logs.send(embed2);
+    }
+  });
+  
+  client.login("YOURTOKEN")
